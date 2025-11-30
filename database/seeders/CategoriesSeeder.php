7573288d5a4a06517ac3e8cfa8d75e125419b4ab
Ana Chavez <anano@Ana_PC.doritos2.local>
2025-11-30 17:15:21 -0600

    Otro cambio

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+/*Para insertar el dato en específico en la base de datos, ejecutar el siguiente comando:
+php artisan db:seed --class=CategorySeeder --force
+*/
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        Category::create([
+            'name' => 'Genérico',
+            'description' => 'Genérico.'
+        ]);
+
+
+        Category::create([
+            'name' => 'Económica',
+            'description' => 'Económica.'
+        ]);
+
+
+        Category::create([
+            'name' => 'Bebidas',
+            'description' => 'Productos bebibles.'
+        ]);
+    }
+}
